c5efb9c8743cf5f905b1d1e9d996b815417e21b5
Raj-mahto <raj.mahto@rthetapi.com>
2025-02-08 16:54:53 +0530

    Security

--- shop-smart-k4/src/main/java/com/example/shop_smart_k4/ShopSmartK4Application.java
+++ shop-smart-k4/src/main/java/com/example/shop_smart_k4/ShopSmartK4Application.java
@@ -8,6 +8,7 @@ public class ShopSmartK4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopSmartK4Application.class, args);
+		System.out.println("Started...");
 		
 	}
 
