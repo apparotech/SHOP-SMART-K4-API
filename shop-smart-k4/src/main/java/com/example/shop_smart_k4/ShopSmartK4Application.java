1016acd8d57d2a9d8bc92e6043f58b350f609af6
Raj-mahto <raj.mahto@rthetapi.com>
2025-02-08 15:18:36 +0530

    Project setup

---
+++ shop-smart-k4/src/main/java/com/example/shop_smart_k4/ShopSmartK4Application.java
@@ -0,0 +1,14 @@
+package com.example.shop_smart_k4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSmartK4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSmartK4Application.class, args);
+		
+	}
+
+}
---
+++ shop-smart-k4/src/test/java/com/example/shop_smart_k4/ShopSmartK4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_smart_k4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartK4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
